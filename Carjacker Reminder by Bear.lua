b5c9df8eadf03eb8a8308c5d3db2bea98a55121e
Bear-HZ <113594957+Bear-HZ@users.noreply.github.com>
2023-01-16 22:35:44 +0530

    Beta-3 upload

--- Carjacker Reminder by Bear.lua
+++ Carjacker Reminder by Bear.lua
@@ -5,8 +5,8 @@
 
 script_name("Carjacker Reminder by Bear")
 script_author("Bear")
-script_version("1.0.0-beta")
-local script_version = "1.0.0-beta-2"
+script_version("1.0.0-beta-3")
+local script_version = "1.0.0-beta-3"
 
 
 -----------------------------------------------------
@@ -64,11 +64,13 @@ local isCarjackerGameTextIntercepted = false
 
 local isCommandAttemptRedundant = false
 
-local hasCharacterLoginOccured = false
-
 local isSellingAvailable = false
 
 local isKCPRequested = false
+
+local hasFirstCharacterLoginOccured = false
+
+local hasNonfirstCharacterLoginOccured = false
 
 
 -----------------------------------------------------
@@ -79,13 +81,30 @@ local isKCPRequested = false
 function sampev.onDisplayGameText(_, _, gameText)
 	if string.find(sampGetCurrentServerName(), "Horizon Roleplay") then
 		if gameText == "~w~Car Selling ~n~~r~Drop the car at the Crane" then
-			hasCharacterLoginOccured = true
+			hasFirstCharacterLoginOccured = true
 			
 			if isCommandResponseAwaited then
 				isCarjackerGameTextIntercepted = true
-				return false
+				return false -- Prevents the "CAR SELLING" banner text from forming only if /sellcar is entered by the mod
 			end
 		end
+	end
+end
+
+function sampev.onSetCheckpoint(checkpointPosition, checkpointRadius)
+	if
+		isCommandResponseAwaited
+		and math.floor(checkpointPosition.x) == 2695
+		and math.floor(checkpointPosition.y) == -2226
+		and math.floor(checkpointPosition.z) == 13
+		and math.floor(checkpointRadius) == 8
+	then
+		-- Prevents the checkpoint from forming server-side only if /sellcar is entered by the mod
+		isKCPRequested = true
+		sampSendChat("/kcp")
+		
+		-- Prevents the checkpoint from forming client-side only if /sellcar is entered by the mod
+		return false
 	end
 end
 
@@ -93,21 +112,22 @@ function sampev.onServerMessage(_, msg_text)
 	if string.find(sampGetCurrentServerName(), "Horizon Roleplay") then
 		-- (Vehicle delivery done) "You sold a car for $600, your reload time is 12 minutes."
 		if msg_text:sub(1, 20) == "You sold a car for $" then
+			hasFirstCharacterLoginOccured = true
 			isSellingAvailable = false
-			hasCharacterLoginOccured = true
 		
 		-- (Checkpoint already exists) "Please ensure that your current checkpoint is destroyed first (you either have material packages, or another existing checkpoint)."
 		elseif string.sub(msg_text, 1, 56) == "Please ensure that your current checkpoint is destroyed " then
-			hasCharacterLoginOccured = true
+			hasFirstCharacterLoginOccured = true
 			
 			if isCommandResponseAwaited then
 				isCommandAttemptRedundant = true
+				isCommandResponseAwaited = false
 				return false
 			end
 		
 		-- (Cooldown ongoing)
 		elseif msg_text == "   You have already dropped a car, wait until your reload time is over!" then
-			hasCharacterLoginOccured = true
+			hasFirstCharacterLoginOccured = true
 			
 			if isCommandResponseAwaited then
 				isCommandResponseAwaited = false
@@ -116,7 +136,7 @@ function sampev.onServerMessage(_, msg_text)
 		
 		-- (Checkpoint killed)
 		elseif msg_text == "All current checkpoints, trackers and accepted fares have been reset." then
-			hasCharacterLoginOccured = true
+			hasFirstCharacterLoginOccured = true
 			
 			if isKCPRequested then
 				isKCPRequested = false
@@ -125,8 +145,15 @@ function sampev.onServerMessage(_, msg_text)
 		
 		-- (Character login) "Welcome to Horizon Roleplay, ..."
 		elseif string.sub(msg_text, 1, 29) == "Welcome to Horizon Roleplay, " then
-			hasCharacterLoginOccured = true
-		
+			hasFirstCharacterLoginOccured = true
+			hasNonfirstCharacterLoginOccured = true
+			
+			-- Stopping command submission and reminder, and resetting some variables, if the server disconnects
+			isCarjackerGameTextIntercepted = false
+			isCommandAttemptRedundant = false
+			isSellingAvailable = false
+			isCommandResponseAwaited = false
+			isKCPRequested = false
 		end
 	end
 end
@@ -174,32 +201,21 @@ function main()
 	-- ADDITIONAL THREADS
 	---------------------
 	
-	--- Stopping command submission and reminder if the server disconnects
-	lua_thread.create(function()
-		while true do
-			if not string.find(sampGetCurrentServerName(), "Horizon Roleplay") then
-				hasCharacterLoginOccured = false
-				isSellingAvailable = false
-				isCommandResponseAwaited = false
-			end
-			
-			wait(100)
-		end
-	end)
-	
 	-- Recurring timer for pulse triggering, determined in length by pulse frequency
 	local isPulseTimerOver
 	lua_thread.create(function()
 		while true do
 			while isSellingAvailable and config_table.Options.isReminderEnabled do
 				isPulseTimerOver = false
-				--local pulseCooldown = (pulseFrequency * 1000) - 500
+				
+				-- Cooldown between pulses
 				wait((pulseFrequency * 1000) - 500)
+				
 				isPulseTimerOver = true
 				wait(500) -- Allowing enough time for the variable to be caught in its true state
 			end
 		
-		wait(1000)
+		wait(100)
 		end
 	end)
 	
@@ -207,77 +223,82 @@ function main()
 	-- MAIN THREAD CONTINUED
 	------------------------
 	
+	repeat wait(100) until hasFirstCharacterLoginOccured -- Detecting the first login
+	
 	while true do
-		while true do
-			repeat wait(100) until hasCharacterLoginOccured and config_table.Options.isReminderEnabled
-			
-			isCommandResponseAwaited = true
-			isCarjackerGameTextIntercepted = false
-			isCommandAttemptRedundant = false
-			
-			sampSendChat("/sellcar")
-			
-			while isCommandResponseAwaited do
-				if isCarjackerGameTextIntercepted then
-					
-					isKCPRequested = true
-					sampSendChat("/kcp")
-					
-					isCommandResponseAwaited = false
-					break
-				end
-				
-				if isCommandAttemptRedundant then
-					isCommandResponseAwaited = false
-					break
-				end
-				
-				wait(10)
+		::start::
+		repeat wait(100) until config_table.Options.isReminderEnabled
+		
+		isCommandResponseAwaited = true
+		isCarjackerGameTextIntercepted = false
+		isCommandAttemptRedundant = false
+		
+		sampSendChat("/sellcar")
+		
+		while isCommandResponseAwaited do
+			if isCarjackerGameTextIntercepted then
+				wait(100) -- a buffer to ensure that the carjacker checkpoint is intercepted by sampev.onSetCheckpoint() before closing the response window
+				isCommandResponseAwaited = false
+				break
 			end
 			
-			if isCarjackerGameTextIntercepted or isCommandAttemptRedundant then
-				isSellingAvailable = true
-				
-				repeat wait(100) until isSellingAvailable and config_table.Options.isReminderEnabled
-				
-				-- Start the reminder
-				createReminderTextdraw()
-				
-				while isSellingAvailable and config_table.Options.isReminderEnabled do
-					-- Pulsing routine
-					if isPulseTimerOver then
-						for i = pulseCount, 1, -1 do
-							wait(pulseDuration / 2)
-							
-							if isSellingAvailable and config_table.Options.isReminderEnabled then
-								sampTextdrawSetBoxColorAndSize(517, 1, 0xFFFFFFFF, 0, game_resY * textSize * #reminderMessage / 90)
-								sampTextdrawSetLetterSizeAndColor(517, textSize / 4, textSize, 0xFF000000)
-								sampTextdrawSetOutlineColor(517, 1, 0xFFFFFFFF)
-							else break
-							end
-							
-							wait(pulseDuration / 2)
-							
-							if isSellingAvailable and config_table.Options.isReminderEnabled then
-								sampTextdrawSetBoxColorAndSize(517, 1, 0xFF000000, 0, game_resY * textSize * #reminderMessage / 90)
-								sampTextdrawSetLetterSizeAndColor(517, textSize / 4, textSize, 0xFFFFFFFF)
-								sampTextdrawSetOutlineColor(517, 1, 0xFF000000)
-							else break
-							end
-						end
-					end
-					
-					wait(100)
-				end
-				
-				sampTextdrawDelete(517)
-				
-			else
-				wait(10000)
-			end
+			wait(0)
 		end
 		
-		wait(0)
+		-- If the player is reconnected, this if statement restarts the testing process quicker, skipping the 10-second pause
+		if hasNonfirstCharacterLoginOccured then
+			hasNonfirstCharacterLoginOccured = false
+			goto start
+		end
+		
+		if isCarjackerGameTextIntercepted or isCommandAttemptRedundant then
+			isSellingAvailable = true
+			
+			-- Loop if the reminder is disabled by the player
+			while isSellingAvailable and not config_table.Options.isReminderEnabled do wait(100) end
+			
+			-- In case the loop was exited due to reconnection
+			if hasNonfirstCharacterLoginOccured then
+				hasNonfirstCharacterLoginOccured = false
+				goto start
+			end
+			
+			-- Start the reminder
+			createReminderTextdraw()
+			
+			while isSellingAvailable and config_table.Options.isReminderEnabled do
+				-- Pulsing routine
+				if isPulseTimerOver then
+					for i = pulseCount, 1, -1 do
+						wait(pulseDuration / 2)
+						
+						if isSellingAvailable and config_table.Options.isReminderEnabled then
+							sampTextdrawSetBoxColorAndSize(517, 1, 0xFFFFFFFF, 0, game_resY * textSize * #reminderMessage / 90)
+							sampTextdrawSetLetterSizeAndColor(517, textSize / 4, textSize, 0xFF000000)
+							sampTextdrawSetOutlineColor(517, 1, 0xFFFFFFFF)
+						else break
+						end
+						
+						wait(pulseDuration / 2)
+						
+						if isSellingAvailable and config_table.Options.isReminderEnabled then
+							sampTextdrawSetBoxColorAndSize(517, 1, 0xFF000000, 0, game_resY * textSize * #reminderMessage / 90)
+							sampTextdrawSetLetterSizeAndColor(517, textSize / 4, textSize, 0xFFFFFFFF)
+							sampTextdrawSetOutlineColor(517, 1, 0xFF000000)
+						else break
+						end
+					end
+				end
+				
+				wait(100)
+			end
+			
+			sampTextdrawDelete(517)
+			
+		else
+			wait(10000)
+		
+		end
 	end
 end
 
